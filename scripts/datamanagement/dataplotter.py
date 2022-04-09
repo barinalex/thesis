2df736eecf577052ab479d9b50b297dca802206e
barinalex <mr.barrri@gmail.com>
2022-04-09 18:17:55 +0200

    directory update

--- scripts/datamanagement/dataplotter.py
+++ scripts/datamanagement/dataplotter.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scripts.datamanagement.datamanagementutils import load_raw_data
-from scripts.constants import DATA_DIR
+from scripts.constants import Dirs
 
-path = f"{DATA_DIR}/real/2022_04_07_15_52_42_186072"
+path = f"{Dirs.realdata}/2022_04_07_15_52_42_186072"
 positions = load_raw_data(path=f"{path}/positions.npy")
 actions = load_raw_data(path=f"{path}/actions.npy")
 linear = load_raw_data(path=f"{path}/linear.npy")
 angular = load_raw_data(path=f"{path}/angular.npy")
 plt.figure()
-plt.plot(np.arange(linear.shape[0]), linear[:, 0])
+plt.plot(np.arange(actions.shape[0]), actions[:, 0])
 plt.show()
