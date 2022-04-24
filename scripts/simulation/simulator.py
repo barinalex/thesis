bb3b06f9dd5a03b58528407a067d95f72ac18caa
barinalex <mr.barrri@gmail.com>
2022-04-24 12:11:43 +0200

    mujoco tuning

--- scripts/simulation/simulator.py
+++ scripts/simulation/simulator.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from scripts.simulation.pybulletvisualizer import Visualizer
 from scripts.simulation.inputwrapper import InputWrapper
-from scripts.engine.modelbased import ModelBased
+from scripts.engine.engine import Engine
 import time
 
 
@@ -11,7 +11,7 @@ class Simulator:
     input: either several models and no trajectory following visualization
     or a single engine with trajectory following feature
     """
-    def __init__(self, iw: InputWrapper, engine: ModelBased):
+    def __init__(self, iw: InputWrapper, engine: Engine):
         self.iw = iw
         self.engine = engine
         self.timestep = engine.state.timestep
@@ -42,12 +42,14 @@ if __name__ == "__main__":
     from scripts.simulation.joystickinputwrapper import JoystickInputWrapper
     from scripts.engine.mlpbased import MLPBased
     from scripts.engine.tcnnbased import TCNNBased
+    from scripts.engine.mujocoengine import MujocoEngine
     from scripts.engine.identityeng import IdentityEng
     from scripts.constants import Dirs
     import os
     path = os.path.join(Dirs.models, "tcnn_2022_04_22_11_27_58_275542")
     # engine = MLPBased(path=path)
-    engine = TCNNBased(path=path, visualize=True)
+    # engine = TCNNBased(path=path, visualize=True)
+    engine = MujocoEngine(visualize=False)
 
     # engine = IdentityEng(datadir="2022_04_12_15_09_00_833808")
 
@@ -60,19 +62,23 @@ if __name__ == "__main__":
     from scripts.datamanagement.datamanagementutils import load_raw_data
     config = loadconfig(f"{path}.yaml")
 
-    limit = 1500
-    path = os.path.join(Dirs.realdata, "2022_04_10_11_57_44_706120")
-    positions = -load_raw_data(path=f"{path}/positions.npy")
-    actions = load_raw_data(path=f"{path}/actions.npy")
-    linear = load_raw_data(path=f"{path}/linear.npy")
-    angular = load_raw_data(path=f"{path}/angular.npy")
-
-    sim = Simulator(iw=DataWrapper(actions=actions[:limit]), engine=engine)
-    simpositions = sim.simulate()
+    limit = 800
+    episodes = ["2022_04_10_11_57_44_706120", "2022_04_10_12_13_40_250118"]
 
     import matplotlib.pyplot as plt
-    plt.figure()
-    plt.plot(positions[:limit, 0], positions[:limit, 1], color='b')
-    plt.plot(simpositions[:, 0], simpositions[:, 1], color='r')
-    plt.legend(['gt', 'sim'])
+    figure, axis = plt.subplots(1, len(episodes))
+    for i, episode in enumerate(episodes):
+        path = os.path.join(Dirs.realdata, episode)
+        positions = -load_raw_data(path=f"{path}/positions.npy")
+        actions = load_raw_data(path=f"{path}/actions.npy")
+        linear = load_raw_data(path=f"{path}/linear.npy")
+        angular = load_raw_data(path=f"{path}/angular.npy")
+        sim = Simulator(iw=DataWrapper(actions=actions[:limit]), engine=engine)
+        simpositions = sim.simulate()
+        engine.reset()
+
+        # axis[i].legend(['gt', 'sim'])
+        axis[i].set_xlabel("meters")
+        axis[i].plot(positions[:limit, 0], positions[:limit, 1], color='b')
+        axis[i].plot(simpositions[:, 0], simpositions[:, 1], color='r')
     plt.show()
