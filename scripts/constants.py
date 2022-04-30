eafccdc66e6d6d2f6c01c5d6e7aeebc5fc37f56e
barinalex <mr.barrri@gmail.com>
2022-04-30 19:10:32 +0200

    refactoring

--- scripts/constants.py
+++ scripts/constants.py
@@ -23,15 +23,16 @@ class Dirs:
     """
     source = os.path.dirname(os.path.abspath(__file__))
     root = os.path.abspath(os.path.join(source, os.pardir))
-    data = root + "/data"
-    realdata = data + "/real2"
-    simdata = data + "/sim"
-    datasets = data + "/datasets"
-    policy = data + "/policy"
-    models = data + "/models"
-    configs = root + "/configs"
-    trajectories = data + "/trajectories"
-    urdf = root + "/pybulletmodel/urdf"
+    data = os.path.join(root, "data")
+    realdata = os.path.join(data, "real")
+    experiments = os.path.join(data, "experiments")
+    simdata = os.path.join(data, "sim")
+    datasets = os.path.join(data, "datasets")
+    policy = os.path.join(data, "policy")
+    models = os.path.join(data, "models")
+    configs = os.path.join(root, "configs")
+    trajectories = os.path.join(data, "trajectories")
+    urdf = os.path.join(root, "pybulletmodel", "urdf")
 
 
 if __name__ == "__main__":
