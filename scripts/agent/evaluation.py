e1140480bb7d4da146cdfe1b51b2e34a28cbc94b
barinalex <mr.barrri@gmail.com>
2022-05-15 13:41:45 +0200

    mjc policy eval

--- scripts/agent/evaluation.py
+++ scripts/agent/evaluation.py
@@ -85,7 +85,7 @@ def evaluate_mujoco_based(n: int, pname: str = "mjc_ppo_2022_05_05_18_07_46_9728
 
     :return: list of rewards for each episodes
     """
-    engine = MujocoEngine(visualize=False)
+    engine = MujocoEngine(visualize=True)
     config = loadconfig(os.path.join(Dirs.configs, "env.yaml"))
     config["trajectories"] = "n10_wps500_smth50_mplr10.npy"
     # config["trajectories"] = "inf_pd02_r1.npy"
@@ -175,11 +175,11 @@ def evaluate_experiments():
 
 
 if __name__ == "__main__":
-    # mujoco_rws = evaluate_mujoco_based(n=10)
-    # print(mujoco_rws)
+    mujoco_rws = evaluate_mujoco_based(pname="best_model", n=1)
+    print(mujoco_rws)
     # mlp_rws = evaluate_mlp_based(n=1)
     # print(mlp_rws)
-    compare_custom2mujoco_based(n=10)
+    # compare_custom2mujoco_based(n=10)
     # history["act"] = np.asarray(history["act"])
     # import matplotlib.pyplot as plt
     # plt.figure()
